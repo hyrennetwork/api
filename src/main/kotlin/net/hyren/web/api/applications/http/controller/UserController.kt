61e2654c332aed73cf2c8d8652f51459b66fdf45
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-13 14:13:28 -0300

    dev

--- src/main/kotlin/net/hyren/web/api/applications/http/controller/UserController.kt
+++ src/main/kotlin/net/hyren/web/api/applications/http/controller/UserController.kt
@@ -1,7 +1,8 @@
 package net.hyren.web.api.applications.http.controller
 
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.jsonMapper
 import com.redefantasy.core.shared.CoreProvider
-import com.redefantasy.core.shared.users.data.User
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -23,11 +24,16 @@ class UserController {
 	@GetMapping("/{id}")
 	fun show(
 		@PathVariable id: Any
-	): User? {
-		return when (id) {
+	): JsonMapper? {
+		val user = when (id) {
 			is String -> CoreProvider.Cache.Local.USERS.provide().fetchByName(id)
 			is UUID -> CoreProvider.Cache.Local.USERS.provide().fetchById(id)
 			else -> null
+		} ?: return null
+
+		return jsonMapper {
+			user.id
+			user.name
 		}
 	}
 
